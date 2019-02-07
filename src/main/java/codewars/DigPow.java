aab9f2218affda295246710058f8f0c22bad619e
bestils <thebestilspl@gmail.com>
2019-02-07 09:37:02 +0100

    Ended digit power sumator/ started Parse molecular calculator

--- src/main/java/codewars/DigPow.java
+++ src/main/java/codewars/DigPow.java
@@ -1,4 +1,31 @@
 package codewars;
-
 public class DigPow {
+
+    public static long digPow(int n, int p) {
+
+        int suma = 0;
+        String liczba = (Integer.toString(n));
+        for (int i = 0; i < liczba.length(); i++) {
+
+            int w = liczba.charAt(i) - '0';
+            suma += Math.pow(w, p + i);
+
+
+        }
+        if (n > suma) {
+            if (n % suma == 0)
+                return n / suma;
+            else
+                return -1;
+        }
+
+    else{
+
+        if (suma % n == 0)
+            return suma / n;
+        else
+            return -1;
+    }
+
+    }
 }
---
+++ src/main/java/codewars/ParseMolecule.java
@@ -0,0 +1,13 @@
+package codewars;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class ParseMolecule {
+
+    public static Map<String,Integer> getAtoms(String formula) {
+        // Your code here!
+
+        return new HashMap<String,Integer>();
+    }
+}
