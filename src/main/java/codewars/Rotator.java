861d06d939e6698c0fdb0a2c0817a91cbedfa336
bestils <thebestilspl@gmail.com>
2019-04-07 14:51:55 +0100

    someAlgorithms comes

--- src/main/java/codewars/Rotator.java
+++ src/main/java/codewars/Rotator.java
@@ -1,4 +1,26 @@
 package codewars;
 
 public class Rotator {
+
+    public Object[] rotate(Object[] data, int n) {
+        int position = 5-(n % data.length);
+        if ( n>15){
+            position=(position+2)%data.length;
+
+        } else if ( n>10){
+            position=(position+1)%data.length;
+
+        }
+        Object[] answerData = new Object[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            if (position > (-1)) {
+                answerData[i] = data[(i + position) % data.length];
+            } else {
+                answerData[i] = data[(i + data.length + position) % data.length];
+            }
+
+        }
+        return answerData;
+    }
 }
