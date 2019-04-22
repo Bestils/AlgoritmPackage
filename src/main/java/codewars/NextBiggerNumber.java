b5fecb9c9d333921843e085edd37fd35d6c019a3
bestils <thebestilspl@gmail.com>
2019-04-22 20:10:20 +0200

    nextBiggerNumber

--- src/main/java/codewars/NextBiggerNumber.java
+++ src/main/java/codewars/NextBiggerNumber.java
@@ -2,8 +2,11 @@ package codewars;
 
 public class NextBiggerNumber {
     public static int nextBiggerNumber(int i) {
+        String iAsString =Integer.toString(i);
+        char[] array=new char[iAsString.length()];
+
         for (char c : Integer.toString(i).toCharArray()) {
-            
+
         }
     }
 }
